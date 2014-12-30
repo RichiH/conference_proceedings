71363e837e920f470a7bf5b2d54e3c97e888fece
Mert Dirik <mertdirik@gmail.com>
2014-12-30 16:06:21 -0500

    Fix debian-meetings 2008

---
+++ Debian/2008/fosdem/encode.sh
@@ -0,0 +1,1 @@
+../../../.git/annex/objects/vK/z5/URL-s4098--http&c%%meetings-archive.debian.net%pub%debian-meetings%2008%fosdem%encode.sh/URL-s4098--http&c%%meetings-archive.debian.net%pub%debian-meetings%2008%fosdem%encode.sh
