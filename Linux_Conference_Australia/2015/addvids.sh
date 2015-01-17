9adb18217afd3918a2388ba9b409cac6e3895f9d
Joey Hess <joeyh@joeyh.name>
2015-01-17 12:36:24 -0400

    another load of LCA

--- Linux_Conference_Australia/2015/addvids.sh
+++ Linux_Conference_Australia/2015/addvids.sh
@@ -4,7 +4,7 @@ for topdir in OGGB_260098 OGGB_FP OGGB4 OGGB3 Case_Room_3 Case_Room_2; do
 		(
 			cd $topdir/$day
 			url=http://mirror.linux.org.au/pub/linux.conf.au/2015/$topdir/$day/
-			git annex addurl --relaxed $(curl $url |grep \.webm|grep href=|sed 's/.*a href=\"//'|sed 's/".*//' | perl -ne "print q{$url}.\$_")
+			git annex addurl --relaxed $(curl $url |grep \.webm|grep href=|sed 's/.*a href=\"//'|sed 's/".*//' | perl -ne "print q{$url}.\$_") -c  annex.alwayscommit=false
 		)
 	done
 done
