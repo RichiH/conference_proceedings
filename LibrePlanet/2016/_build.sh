d700d24fd15c62560d9f14271da446a108cc473c
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2016-03-20 03:45:36 +0100

    LibrePlanet/2016: Prerelease of Snowden keynote

---
+++ LibrePlanet/2016/_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+THIS_DIR=${BASH_SOURCE[0]%/*}
+BASEURL=http://media.libreplanet.org/u/libreplanet/tag/lp2016
+
+cd "$THIS_DIR"
+
+wget -O _index.html.new ${BASEURL}/
+mv _index.html.new _index.html
+
+for url in $(./_extract_video_urls.py _index.html); do
+  git annex addurl --relaxed --pathdepth=-1 "$url"
+done
