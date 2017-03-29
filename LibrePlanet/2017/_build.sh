02003ba38a1f9301738a14a66e2a1bd4c1eb7ac8
Joey Hess <joeyh@joeyh.name>
2017-03-29 12:27:48 -0400

    first set of LibrePlanet 2017 videos
    Named by presented last name, which is a bit suboptimal

---
+++ LibrePlanet/2017/_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+THIS_DIR=${BASH_SOURCE[0]%/*}
+BASEURLS=( http://media.libreplanet.org/u/libreplanet/tag/{lp,libreplanet-}2017 )
+
+cd "$THIS_DIR"
+
+for baseurl in ${BASEURLS[@]}; do
+  wget -O _index.html.new ${baseurl}/
+  mv _index.html.new _index.html
+
+  for url in $(./_extract_video_urls.py _index.html); do
+    git annex addurl --relaxed --pathdepth=-1 "$url"
+  done
+done
