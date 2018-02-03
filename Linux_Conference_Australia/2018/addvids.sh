906e21b72fe9437ff6c092857952239080c5a3d2
Joey Hess <joeyh@joeyh.name>
2018-02-03 18:07:41 -0400

    added LCA developers miniconf

--- Linux_Conference_Australia/2018/addvids.sh
+++ Linux_Conference_Australia/2018/addvids.sh
@@ -1,6 +1,6 @@
 baseurl=http://mirror.linux.org.au/pub/linux.conf.au/2018
 for day in Monday Tuesday Wednesday Thursday Friday; do
-	for room in collab greathall green gutherie largepod medium; do
+	for room in collab greathall green guthrie largepod medium; do
 		url="$baseurl/$room/$day/"
 		git annex addurl --pathdepth=-3 --relaxed $(curl $url |grep \.mp4|grep href=|sed 's/.*a href=\"//'|sed 's/".*//' | perl -ne "print q{$url}.\$_") -c  annex.alwayscommit=false
 	done
