7b76615b903b6aec4269a04f053accc9093736b0
Joey Hess <joeyh@joeyh.name>
2018-02-17 20:34:11 -0400

    re-imported debconf17
    initial import was merged w/o git-annex metadata

--- Debian/fetch-from-ftp.sh
+++ Debian/fetch-from-ftp.sh
@@ -4,6 +4,6 @@ url=$1
 
 for file in $(curl "$url" | grep 'a href' | grep -v 'Parent Directory' | sed 's/^.*href="//;s/">.*//'); do
     if [ ! -h "$file" ] ; then
-        git annex addurl --relaxed --file "$file" "$url$file"
+        git -c annex.alwayscommit=false annex addurl --relaxed --file "$file" "$url$file"
     fi
 done
